795c4d44d6d0f68f45f161a7fcaae7801f357e48
lhshilin <1352058684@qq.com>
2024-02-17 14:30:58 +0800

    修改发送邮箱文字格式

--- src/api/sendMail.js
+++ src/api/sendMail.js
@@ -7,19 +7,14 @@ let spliceInfo;
 export async function sendMail(to, title, text) {
     if (!spliceInfo) {
         await axios.get("https://api.vvhan.com/api/visitor.info").then(({ data }) => {
-            spliceInfo = `<p>评论人ip地址：${data.ip}</p>
-            <p>评论人地址：${data.location}</p>
-            <p>使用浏览器：${data.browser}</p>
-            <p>浏览器版本：${data.browser_ver}</p>
-            <p>操作系统：${data.system}</p>
-            `;
+            spliceInfo = `<p>评论人ip地址：${data.ip}</p><p>评论人地址：${data.location}</p><p>使用浏览器：${data.browser}</p><p>浏览器版本：${data.browser_ver}</p><p>操作系统：${data.system}</p>`;
         })
     }
     return await request.get("https://api.wer.plus/api/qqmail", {
         params: {
             name: "个人博客评论", // 邮箱发件人名
-            me: "198513441@qq.com", // 邮箱发件人邮箱号
-            key: "dtishihifcobbjeb", // 发件人授权码
+            me: "1352058684@qq.com", // 邮箱发件人邮箱号
+            key: "iiduvxvduptdgdgf", // 发件人授权码
             to, // 向谁发邮箱
             title, // 邮箱标题
             text: text + spliceInfo // 邮箱内容
@@ -31,9 +26,9 @@ export async function sendMail(to, title, text) {
     //         Host: "smtp.qq.com", // SMTP服务器
     //         Port: 465, // 服务器端口 25 或者465 具体要看邮箱服务器支持，QQ邮箱可以填465
     //         SMTPSecure: "ssl", // 服务器端口 25 或者465 具体要看邮箱服务器支持，QQ邮箱可以填465
-    //         Username: "198513441@qq.com", // 发件人邮箱
+    //         Username: "1352058684@qq.com", // 发件人邮箱
     //         name: "个人博客评论", // 邮箱发件人名
-    //         Password: "dtishihifcobbjeb", // 发件人邮箱授权码
+    //         Password: "iiduvxvduptdgdgf", // 发件人邮箱授权码
     //         addAddress: to, // 收件人邮箱
     //         title, // 发送的邮箱标题
     //         text: text + spliceInfo // 发送的邮箱内容
--- src/components/MessageArea/DataForm.vue
+++ src/components/MessageArea/DataForm.vue
@@ -136,13 +136,16 @@ export default {
 
                 // 评论内容
                 const blogName = isData.blog
-                  ? `<p>评论的文章名：${isData.blog.title}</p>评论的文章id：${isData.blog.id}`
+                  ? `评论的文章名：${isData.blog.title}`
+                  : "";
+                const blogId = blogName
+                  ? `评论的文章id：${isData.blog.id}`
                   : "";
                 const nickname = `评论者昵称：${isData.nickname}`;
+                const content = `评论者内容：${isData.content}`;
                 const identity = identityInfo
                   ? `评论者身份：${identityInfo.identity}`
                   : "";
-                const content = `评论者内容：${isData.content}`;
                 const commentId = `评论id：${isData.id}`;
                 const createDate = `评论时间：${formatDate(
                   isData.createDate,
@@ -150,13 +153,7 @@ export default {
                 )}`;
 
                 // 评论的内容--评论者的信息identity
-                const mailContent = `<p>${blogName}</p>
-                <p>${nickname}</p>
-                <p>${identity}</p>
-                <p>${content}</p>
-                <p>${commentId}</p>
-                <p>${createDate}</p>
-                `;
+                const mailContent = `<p>${blogName}</p><p>${blogId}</p><p>${nickname}</p><p>${content}</p><p>${identity}</p><p>${commentId}</p><p>${createDate}</p>`;
 
                 sendMail(this.setting.mail, mailTitle, mailContent);
               }
